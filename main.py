e6b9f7c1dcbe7a14e81530db9438e0109f08dd1d
davcika <daviskovals6@gmail.com>
2025-11-23 01:41:49 +0200

    seperated word count function from main into stats.py

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
+from stats import get_num_words
+
 def get_book_text(path):
     with open(path) as f:
         return f.read()
 
 def main():
-    print(get_book_text("books/frankenstein.txt"))
+    book_text = get_book_text("books/frankenstein.txt")
+    print(f"Found {get_num_words(book_text)} total words")
 
 main()

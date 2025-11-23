a58fea0eb1daf55195e2339010e88660abd6c3f7
davcika <daviskovals6@gmail.com>
2025-11-23 02:32:39 +0200

    added pretty_sort and pretty_print functions

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import get_num_words, count_letters
+from stats import get_num_words, count_letters, pretty_sort
 
 def get_book_text(path):
     with open(path) as f:
@@ -8,6 +8,6 @@ def main():
     book_text = get_book_text("books/frankenstein.txt")
     letter_dict = count_letters(book_text)
     print(f"Found {get_num_words(book_text)} total words")
-    print(letter_dict)
+    pretty_sort(letter_dict)
 
 main()
--- stats.py
+++ stats.py
@@ -10,3 +10,16 @@ def count_letters(text):
         else:
             mydict[i] = 1
     return mydict
+
+def pretty_sort(dict_s):
+    sorted_dict = dict(sorted(dict_s.items(), key=lambda item:item[1], reverse=True))
+    
+def pretty_print(path, word_count, sorted_dict):
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {path}...")
+    print("----------- Word Count ----------")
+    print(f"Found {word_count} total words")
+    print("--------- Character Count -------")
+    for i in sorted_dict:
+        print(f"{i}: {sorted_dict[i]}")
+    print("============= END ===============")
